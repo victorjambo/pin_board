ef9306dd2c82b225c4170167f2277e8c789008ce
victorjambo <victorjambo@live.com>
2017-02-09 23:57:37 +0300

    CRUD styles users

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,59 @@
+class PinsController < ApplicationController
+	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
+	before_action :authenticate_user!, except: [:index, :show]
+
+	def index
+		@pins = Pin.all.order("created_at DESC")
+	end
+
+	def new
+		@pin = current_user.pins.build
+	end
+
+	def create
+		@pin = current_user.pins.build(pin_params)
+
+		if @pin.save
+			redirect_to @pin, notice: "Successfully created new pin"
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		
+	end
+
+	def update
+		if @pin.update(pin_params)
+			redirect_to @pin, notice: "Successfully updated"
+		else
+			render 'edit'				
+		end
+	end
+
+	def destroy
+		@pin.destroy
+		redirect_to root_path
+	end
+
+	def edit
+		
+	end
+
+	def upvote
+		@pin.upvote_by current_user
+		redirect_to :back
+	end
+
+	private
+
+	def pin_params
+		params.require(:pin).permit(:title, :description, :image)
+	end
+
+	def find_pin
+		@pin = Pin.find(params[:id])
+	end
+
+end
